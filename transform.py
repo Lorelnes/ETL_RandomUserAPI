80e2f4be4fc5c146ebf16f2900b9a81e8e7748c4
DavidTchanturia <davit.tchanturia.1@btu.edu.ge>
2024-07-03 11:05:52 +0400

    doc: feedback for random user ETL, including performance improvemnts, code readability, design and future considerations

--- transform.py
+++ transform.py
@@ -1,31 +1,28 @@
-from constants import URL
-from phonenumbers import geocoder
+from constants import URL # unused
+from phonenumbers import geocoder # unused
 from phonenumbers.phonenumberutil import NumberParseException
-from dataclasses import dataclass, field
+from dataclasses import dataclass, field # unused import
 from typing import List, Dict, Optional
 from geopy.geocoders import Nominatim
 from pydantic import BaseModel, EmailStr
 import pandas as pd
-import datetime
+import datetime # unused
 import phonenumbers
-import time
+import time # unused
 
 
 def full_name(df: pd.DataFrame) -> pd.DataFrame:
     '''
     This function takes 'first' and 'last' keys from 'name' dict and puts them together to replace
     'name' column with first name and last name.
-
-    Args:
-        df
-
-    Returns:
-        df
     '''
     df['name'] = df['name'].apply(lambda x: f"{x.get('first', '')} {x.get('last', '')}")
     return df
 
 def get_location(row: Dict) -> Optional[str]:
+    # what is row and where did it come from
+    # it would be optional if you pass lan and lon as arguments to the function and return the plance
+    # later this function can be used to apply it on a df
     '''
     This function extracts latitude and longitude from the 'coordinates' dictionary
     in the 'location' column.
@@ -43,8 +40,17 @@ def get_location(row: Dict) -> Optional[str]:
     latitude = location_data.get('coordinates', {}).get('latitude')
     longitude = location_data.get('coordinates', {}).get('longitude')
 
+    # this expression can be simplified
     if latitude is None and longitude is None:
         return None
+    
+    # if latitude and longitude
+        # geolocator =Nominatim(user_agent='geoapieExercises', timeout=20)
+        # coordinates = f"{latitude},{longitude}"
+        # Location = geolocator.geocode(coordinates)
+
+        # return Location
+    # return None
 
     geolocator =Nominatim(user_agent='geoapieExercises', timeout=20)
     coordinates = f"{latitude},{longitude}"
@@ -53,6 +59,7 @@ def get_location(row: Dict) -> Optional[str]:
     return Location
 
 
+# functions name is not clear, will confuse other developers
 def initials(name: str) -> str:
     '''
     This function generates initials based on 'name' column.
@@ -96,7 +103,7 @@ def parsing_phoneloc(df: pd.DataFrame) -> pd.DataFrame:
         The modified DataFrame with the 'phoneloc' column.
     '''
     df['phoneloc'] = df[['phone', 'nat']].apply(lambda x: f"{x['phone']}, {x['nat']}", axis=1)
-    df['phoneloc'] = df['phoneloc'].apply(format_phone_number)
+    df['phoneloc'] = df['phoneloc'].apply(format_phone_number) 
     return df
 
 
@@ -108,15 +115,21 @@ def validate_emails(df: pd.DataFrame) -> pd.DataFrame:
 
     Returns: df
     '''
+    #  returns df, args df says nothing. if there is no good information better to remove them
+
+    # this class is defined inside a function and you are not even using it
+    # I do not see a reason why you should be definind a class inside a function, even more a dataclass
+    # if you want to validate your data, create a class outside of a function and use it.
     class UserEmail(BaseModel):
         email: EmailStr
 
     emails = df['email'].tolist()
     for email in emails:
         try:
-
+            #  you are validating the user email and then just returning same email, how is that going to work?
             validated_user = UserEmail(email=email)
         except pydantic.ValidationError as e:
+            # log the error, do not print it
             print(f'Validation Error: {e}')
     return df
 
@@ -124,10 +137,6 @@ def validate_emails(df: pd.DataFrame) -> pd.DataFrame:
 def dob_to_datetime(df: pd.DataFrame) -> pd.DataFrame:
     '''
     This function formats 'dob' column as datetime and creates another column 'date_of_birth'.
-
-    Args: df, which is a dataframe.
-
-    Returns: df.
     '''
     df['date_of_birth'] = df['dob'].apply(lambda x: pd.to_datetime(x['date']))
     return df
@@ -135,10 +144,6 @@ def dob_to_datetime(df: pd.DataFrame) -> pd.DataFrame:
 def reg_to_datetime(df: pd.DataFrame) -> pd.DataFrame:
     '''
     This function formats 'registered' column as datetime and creates another column 'registration_date'.
-
-    Args: df
-
-    Returns: df
     '''
     df['registration_date'] = df['registered'].apply(lambda x: pd.to_datetime(x['date']))
     return df
@@ -146,9 +151,6 @@ def reg_to_datetime(df: pd.DataFrame) -> pd.DataFrame:
 def age_dob(df: pd.DataFrame) -> pd.DataFrame:
     '''
     This function takes 'date' key from 'dob' column and dynamically calculates the age of the user, then creates 'age' column separately.
-
-    Args: df
-    Returns: df.
     '''
 
     today = pd.Timestamp.today().year
@@ -160,9 +162,6 @@ def age_user(df: pd.DataFrame) -> pd.DataFrame:
     '''
     This function takes 'age' key from 'registered' column and dynamically calculates the registration age of the user,
     then creates 'age_as_user' column separately.
-
-    Args: df
-    Returns: df.
     '''
     today = pd.Timestamp.today().year
     df['registered'] = df['registered'].apply(lambda x: pd.to_datetime(x['date']))
@@ -187,6 +186,7 @@ def reorder_cols(df: pd.DataFrame) -> pd.DataFrame:
     Args: df
     Returns: df.
     '''
+    #  these are the columns you need, better to have it in the separate file as constants, easier to use
     reordered = ['gender', 'name', 'initials', 'location', 'date_of_birth', 'age', 'registration_date', 'age_as_user',
                  'email', 'phone', 'nat']
 
