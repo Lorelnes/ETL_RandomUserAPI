35d993a73af66d4d0dbdb30c75dfee6fa2d3c3c1
Lorelnes <nataananidze@gmail.com>
2024-07-06 20:38:40 +0400

    Create table using table schema

--- constants.py
+++ constants.py
@@ -1,30 +1,27 @@
 URL = "https://randomuser.me/api"
 
-# this query is hardcoded, try to define schema beforhand and then dinamically join it 
-# for example:
 
-# table_schema = {
-#     "id": "SERIAL PRIMARY KEY",
-#     "gender": "VARCHAR(255)"
-#     -----and so on-----
-# }
+columns_to_drop = ['login', 'cell', 'picture', 'dob', 'registered', 'phoneloc', 'id']
+columns_to_reorder = ['gender', 'name', 'initials', 'location', 'date_of_birth', 'age', 'registration_date', 'age_as_user',
+                      'email', 'phone', 'nat']
 
+table_schema = {
+    "id": "SERIAL PRIMARY KEY",
+    "gender": "VARCHAR(255)",
+    "name": "VARCHAR(255)",
+    "initials": "VARCHAR(255)",
+    "location": "TEXT",
+    "date_of_birth": "DATE",
+    "age": "INTEGER",
+    "registration_date": "DATE",
+    "age_as_user": "INTEGER",
+    "email": "VARCHAR(255)",
+    "phone": "VARCHAR(255)",
+    "nat": "VARCHAR(255)"
+}
 
-create_table_query = """
-CREATE TABLE IF NOT EXISTS RandomUsersAPI (
-                   id SERIAL PRIMARY KEY,
-                   gender VARCHAR(255),
-                   name VARCHAR(255),
-                   initials VARCHAR(255),
-                   location TEXT,
-                   date_of_birth DATE,
-                   age INTEGER,
-                   registration_date DATE,
-                   age_as_user INTEGER,
-                   email VARCHAR(255),
-                   phone VARCHAR(255),
-                   nat VARCHAR(255)
-                   ); """
+create_table_query = sql.SQL("CREATE TABLE IF NOT EXISTS RandomUserAPI ()").format(table_schema)
+
 
 insert_data_query = f"""
 INSERT INTO RandomUsersAPI (gender, name, initials, location, date_of_birth, age, registration_date, age_as_user, email, phone, nat)
@@ -32,6 +29,3 @@ VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
 """
 
 
-# you can have columns to reorder here
-# columns_to_reorder = ['gender', 'name', 'initials', 'location', 'date_of_birth', 'age', 'registration_date', 'age_as_user',
-#                  'email', 'phone', 'nat']
