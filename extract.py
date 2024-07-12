cece1b61155717409dc52b5dd37b39016d915310
Lorelnes <nataananidze@gmail.com>
2024-07-12 18:15:49 +0400

    Correct type hints and docstring for extract.py file

--- extract.py
+++ extract.py
@@ -1,11 +1,11 @@
 from constants import URL
-from typing import Dict, List
+from typing import Dict
 import requests
 import logging
 
 logging.basicConfig(level=logging.INFO)
 
-def extract_one_user(URL: str) -> List[Dict]:
+def extract_one_user(URL: str) -> Dict:
     """
     Fetches data from URL, parses JSON response, and extracts
     the first user data element from 'results' key.
@@ -16,7 +16,6 @@ def extract_one_user(URL: str) -> List[Dict]:
     Returns:
     dict: The first user data element if found, otherwise None.
     """
-    # no need to include raises in doc string, too much commenting can be hard to read, do not do it
 
     try:
         response = requests.get(url=URL)
@@ -35,32 +34,5 @@ def extract_one_user(URL: str) -> List[Dict]:
     except requests.exceptions.RequestException as e:
         logging.error(f"Error fetching data from {URL}: {e}")
 
-def extract_all_users(URL: str) -> List[dict]:
-    """
-    Extracts 1000 user data from the URL and returns dictionary:
 
-    Args:
-         URL(type hint: str) : the URL from which we extract the data.
-
-    Returns:
-         dict: A dictionary containing the extracted data.
-    """
-    # no need to include logs in docstring as well
-
-
-    extracted_users = [] # try not to save users in list and work on each response as you retrieve it, we will do batch processing later
-    for i in range(5): # why hardcoding 5 as the number of users? this should be in the main.py
-        user = extract_one_user(URL)
-        if user:
-            extracted_users.append(user)
-            if extracted_users:
-                logging.info(f"Extracted {len(extracted_users)} users")
-            else:
-                logging.warning("No users extracted")
-
-    return extracted_users
-
-
-# why are you calling the function here, then you are importing extracted_users. call the function in main.py
-extracted_users = extract_all_users(URL)
 
