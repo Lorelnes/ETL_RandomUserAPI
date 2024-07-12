5ddfe4a2557ffb52fa2de0ab80dd4e367a8b1d73
Lorelnes <nataananidze@gmail.com>
2024-07-12 18:26:33 +0400

    Correct docstrings and type hints for load.py file

--- load.py
+++ load.py
@@ -1,7 +1,6 @@
 from constants import create_table_query, insert_data_query
-from extract import *
-from settings import dbname, user, host, password, port # unused
-from psycopg2.extras import execute_values # unused
+from extract import extract_one_user
+from settings import dbname, user, host, password, port
 import pandas as pd
 import psycopg2
 import logging
@@ -16,16 +15,14 @@ def load_to_raw_data(data: list, filename: str = 'all_data.json') -> None:
         json.dump(data, json_file, indent = 4)
         logging.info(f'Data was saved to {filepath}')
 
-def create_table(conn) -> None:
-    # pass the query as an argument, easier to debug and understand
+def create_table(conn, create_table_query) -> None:
     """
       Creates a table in the connected database based on the provided schema.
 
       Args:
           conn: A psycopg2 connection object to the database.
-
+          create_table_query: A psycopg2 query to create the table.
       """
-    # what happens if the table exists?
     try:
         cur = conn.cursor()
         cur.execute(create_table_query)
@@ -33,28 +30,15 @@ def create_table(conn) -> None:
         logging.info(f'Table was created')
     except psycopg2.Error as e:
         logging.error(f"Error creating table: {e}")
-    finally:
-        cur.close()
 
-def load_data_to_database(df: pd.DataFrame, dbname: str, user: str, host: str, password: str, port: int) -> None:
+def load_data_to_database(user_data: Dict, conn) -> None:
     """
-     Loads data from a pandas DataFrame into a table in the specified database.
-
-     Args:
-         df: The pandas DataFrame containing the data to be loaded.
-         dbname: The name of the database.
-         user: The username for database access.
-         host: The hostname or IP address of the database server.
-         password: The password for database access.
-         port: The port number of the database server.
-     """
+     Loads data into a table in the specified database.
+    """
     try:
-       # instead of creating connection every time, create it in main, use for all functions and then close it
-       conn = psycopg2.connect(dbname=dbname, user=user, host=host, password=password, port=port)
-
        cur = conn.cursor()
 
-       cur.executemany(insert_data_query, df.values.tolist())
+       cur.executemany(insert_data_query, [user_data])
 
        conn.commit()
 
@@ -62,5 +46,4 @@ def load_data_to_database(df: pd.DataFrame, dbname: str, user: str, host: str, p
 
     except psycopg2.Error as e:
         logging.error(f"Error occured during data loading: {e}")
-    finally:
-        conn.close()
+
