4f2be243f88e6af20c43e6ff30a988fc1229edce
Lorelnes <nataananidze@gmail.com>
2024-07-15 19:04:08 +0400

    Create table schema

--- constants.py
+++ constants.py
@@ -13,7 +13,7 @@ table_schema = {
     "gender": "VARCHAR(255)",
     "name": "VARCHAR(255)",
     "initials": "VARCHAR(255)",
-    "location": "VARCHAR(MAX)",
+    "location": "TEXT",
     "date_of_birth": "TEXT",
     "age": "INTEGER",
     "registration_date": "DATE",
@@ -23,8 +23,11 @@ table_schema = {
     "nat": "VARCHAR(255)"
 }
 
-create_table_query = sql.SQL("CREATE TABLE IF NOT EXISTS RandomUserAPI ()").format(table_schema)
-
+create_table_query = f"""
+CREATE TABLE IF NOT EXISTS RandomUsersAPI (
+    {', '.join(f'{column} {data_type}' for column, data_type in table_schema.items())}
+);
+"""
 
 insert_data_query = """
 INSERT INTO RandomUsersAPI (gender, name, initials, location, date_of_birth, age, registration_date, age_as_user, email, phone, nat)
